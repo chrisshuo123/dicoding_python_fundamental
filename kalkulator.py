febc821696aa3536f417e7e4c6e86c7f467e1530
chrisshuo123 <chr1z100@gmail.com>
2026-05-06 03:17:49 +0700

    add new 10_2_codeFormatting directory in the readme_image, while continue documenting my learning process in the README.md

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,10 @@
-class kalkulator:
+class Kalkulator:
     """kalkulator tambah kurang"""
+
     def __init__(self, _i):
         self.i = _i
+
     def tambah(self, _i): return self.i + _i
+
     def kurang(self, _i):
-    return self.i - _i
+        return self.i - _i
