0754f7ff7e3bea5b0d9fe7e4e88c7c9edebca434
chrisshuo123 <chr1z100@gmail.com>
2026-05-15 03:38:38 +0700

    re-commit the unsaved keep.py pickle

--- 12_library/12_6_libFileManagement/pickle/keep.py
+++ 12_library/12_6_libFileManagement/pickle/keep.py
@@ -1,6 +1,5 @@
 import pickle
-
-contoh_dictionary = {1:"6", 2:"2", 3:"f"}
-pickle_keluar = open("dict.pickle", "wb")
-pickle.dump(contoh_dictionary, pickle_keluar)
-pickle_keluar.close()
+contoh_dictionary = {1:"6", 2:"2", 3:"f"}  # Dictionary yang akan disimpan
+pickle_keluar = open("dict.pickle","wb")    # Buka file mode write binary
+pickle.dump(contoh_dictionary, pickle_keluar) # Simpan object
+pickle_keluar.close()                        # Tutup file
